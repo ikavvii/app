cdaea052dd260328f36b7f62eed6ea3a0d65d4fe
ikavvii <immkavin.ofcl@gmail.com>
2025-11-19 07:20:58 +0530

    feat: add method-override and error handling middleware

--- src/app.js
+++ src/app.js
@@ -4,11 +4,18 @@ import { fileURLToPath } from 'url'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 import connectDatabase from './config/database.js'
 import studentsRouter from './routes/studentsRoute.js'
+import methodOverride from 'method-override'
+import notFoundHandler from './middlewares/404.js'
+import errorHandler from './middlewares/errorHandler.js'
+
 const app = express()
 
 connectDatabase()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -18,5 +25,8 @@ app.use('/students', studentsRouter)
 app.get('/', (req, res) => {
   res.redirect('/students')
 })
+
+app.use(notFoundHandler)
+app.use(errorHandler)
 
 export default app
