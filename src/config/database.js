7d1fbda11705b512047f41221c08ad37a1ec93ac
ikavvii <immkavin.ofcl@gmail.com>
2025-11-19 20:29:18 +0530

    refactor: simplify database connection logic and improve logging

--- src/config/database.js
+++ src/config/database.js
@@ -1,29 +1,20 @@
-import mongoose, { connect } from 'mongoose'
+import { connect } from 'mongoose'
 import { configDotenv } from 'dotenv'
 configDotenv()
 
 async function connectDatabase () {
   try {
-    await connect(process.env.MONGO_URI)
+    const mongoose = await connect(process.env.MONGO_URI)
 
     console.log('Connected to the MongoDB database successfully!')
+    console.log('Database connected: ', mongoose.connection.name)
+
+    return mongoose.connection
   } catch (error) {
     console.error('Error: ', error.message)
 
     process.exit(1)
   }
-  const dbConnection = mongoose.connection
-  
-  
-  dbConnection.once('open', _ => {
-    console.log('Database connected: ', dbConnection.name)
-  })
-
-  dbConnection.on('error', err => {
-    console.error('Connection error: ', err.message)
-  })
-
-  return dbConnection
 }
 
 export default connectDatabase
